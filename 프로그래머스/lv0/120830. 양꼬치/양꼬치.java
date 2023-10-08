7843181fbf147cad8f385d16d2759ca33b9495ba
xoqls97 <143566450+xoqls97@users.noreply.github.com>
2023-10-09 02:29:16 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int n, int k) {
 		
-		int sheep = 12000;
-		int drink = 2000;
-		int sale = k-(n/10);
-		
-		int result = (n*sheep)+(sale*drink);
-		return result;
+		return ((k-(n/10))*2000)+(n*12000);
     }
 }
