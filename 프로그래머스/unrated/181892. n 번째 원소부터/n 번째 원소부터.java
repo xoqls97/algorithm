9794a639c35b6363061b328647ce4f63082f4733
xoqls97 <143566450+xoqls97@users.noreply.github.com>
2023-10-20 09:50:08 +0900

    [level unrated] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int b [] = Arrays.copyOfRange(num_list,n-1,num_list.length);
+        return b;
+       
+    }
+}
